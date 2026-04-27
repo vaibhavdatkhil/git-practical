e332148f816da8c1906c5062e875f4ffa200949a
vaibhavdatkhil <vaibhavdatkhil@email.com>
2026-04-27 10:46:37 +0530

    app

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello Git Practical")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World! 🚀"
+
+@app.route('/about')
+def about():
+    return "This is my Flask app for exam!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
